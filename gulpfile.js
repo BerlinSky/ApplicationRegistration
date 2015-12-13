118e994d6b9938ab073be7c434b1ab5400304c77
Berlin Sky <pedroyoung8989@gmail.com>
2015-12-13 18:34:21 +0900

    Documentation section

--- gulpfile.js
+++ gulpfile.js
@@ -28,6 +28,8 @@ var config = {
   	jslib: [
 			'bower_components/jquery/dist/jquery.min.js',
 			'bower_components/bootstrap/dist/js/bootstrap.min.js',
+			'bower_components/angular/angular.js',
+			'bower_components/angular-resource/angular-resource.min.js',
 			'bower_components/d3/d3.min.js',
 			'bower_components/jquery.easing/jquery.easing.min.js',
 			'bower_components/angular-ui-router/release/angular-ui-router.min.js',
