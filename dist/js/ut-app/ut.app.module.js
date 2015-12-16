40af9da0db02f344ce76c557252ad118e1d2454d
Berlin Sky <pedroyoung8989@gmail.com>
2015-12-16 12:51:48 +0900

    removal of the obselete files

--- dist/js/ut-app/ut.app.module.js
+++ dist/js/ut-app/ut.app.module.js
@@ -19,44 +19,57 @@
     "ut.dataService",
     "ut.applicationResourceMock"]);
 
-    var registerUiRoutes = function($stateProvider, $urlRouterProvider) {
-      $urlRouterProvider.otherwise("/");
+  module.config(function ($provide) {
+      $provide.decorator("$exceptionHandler",
+          ["$delegate",
+              function ($delegate) {
+                  return function (exception, cause) {
+                      exception.message = "Please contact the Help Desk! \n Message: " +
+                                                              exception.message;
+                      $delegate(exception, cause);
+                      alert(exception.message);
+                  };
+              }]);
+  });
 
-      $stateProvider
-        .state("home", {
-          url: "/",
-          templateUrl: "template/landing.html"
-        })
-        // .state("registration", {
-        //   url: "/registration",
-        //   templateUrl: "template/app-registration.html",
-        //   controller: "UtSearchController as model"
-        // })
-        .state("applicationList", {
-          url: "/applicationList",
-          templateUrl: "js/ut-app/applicationList/appList.html",
-          controller: "UtAppListController as model"
-        })
-        .state("applicationList.info", {
-          url: "/info",
-          templateUrl: "js/ut-app/applicationList/appInfo.html"
-        })
-        .state("applicationList.pages", {
-          url: "/pages",
-          templateUrl: "js/ut-app/applicationPageList/pageList.html",
-          controller: "UtPageListController as modelTable"
-        })
-        .state("applicationList.json", {
-          url: "/json",
-          templateUrl: "js/ut-app/applicationPageList/pageListJson.html",
-          controller: "UtPageListController as modelTable"
-        })
-        .state("applicationList.form", {
-          url: "/json",
-          templateUrl: "js/ut-app/applicationList/appForm.html"
-        })
-    };
+  var registerUiRoutes = function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/");
 
-    module.config(["$stateProvider", "$urlRouterProvider", registerUiRoutes]);
+    $stateProvider
+    .state("home", {
+      url: "/",
+      templateUrl: "template/landing.html"
+    })
+    // .state("registration", {
+    //   url: "/registration",
+    //   templateUrl: "template/app-registration.html",
+    //   controller: "UtSearchController as model"
+    // })
+    .state("applicationList", {
+      url: "/applicationList",
+      templateUrl: "js/ut-app/applicationList/appList.html",
+      controller: "UtAppListController as model"
+    })
+    .state("applicationList.info", {
+      url: "/info",
+      templateUrl: "js/ut-app/applicationList/appInfo.html"
+    })
+    .state("applicationList.pages", {
+      url: "/pages",
+      templateUrl: "js/ut-app/applicationPageList/pageList.html",
+      controller: "UtPageListController as modelTable"
+    })
+    .state("applicationList.json", {
+      url: "/json",
+      templateUrl: "js/ut-app/applicationPageList/pageListJson.html",
+      controller: "UtPageListController as modelTable"
+    })
+    .state("applicationList.form", {
+      url: "/json",
+      templateUrl: "js/ut-app/applicationList/appForm.html"
+    })
+  };
+
+  module.config(["$stateProvider", "$urlRouterProvider", registerUiRoutes]);
 
 }());
